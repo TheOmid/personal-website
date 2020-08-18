c8d33b7a13146413c1960d3eb18010decda07249
omids@DESKTOP-AMJCSCT <omid.shahidi.2012@gmail.com>
2020-08-17 22:17:04 -0400

    refresh fixed 2.0

--- assets/js/refresh.js
+++ assets/js/refresh.js
@@ -1,5 +1,6 @@
-history.scrollRestoration = "manual"
+history.scrollRestoration = "manual";
 
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
+  document.querySelector('html').style.scrollBehavior = '';
 }
