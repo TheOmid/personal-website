9e53a5dc42c4a0698bbfab2a0422b89c3cb17351
omids@DESKTOP-AMJCSCT <omid.shahidi.2012@gmail.com>
2020-10-27 22:52:48 -0400

    new color design. wave animation.

---
+++ assets/js/wave.js
@@ -0,0 +1,30 @@
+//p5.js
+let count = 0;
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight / 5);
+  canvas.parent("wave-animation");
+  background("#201F1F");
+}
+//I hope wavification is a word, but basically drawing the sketch
+function draw() {
+  noFill();
+  
+  if(count < 500){
+    stroke(count / 4, 255 - count * 0.8, 200, 100);
+    beginShape();
+    for(x = 0; x < width; x++){
+      let n = noise(x * 0.005, frameCount * 0.005);
+      let y = map(n, 0, 1, 0, height);
+      curveVertex(x, y);
+    }
+    endShape();
+    count++;
+    
+  }
+}
+
+//loop, every 7 secs a new sketch
+setInterval(() => {
+  count = 0;
+  background("#201F1F");
+}, 7000);
