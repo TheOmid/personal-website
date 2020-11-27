0f3979b18f50e4421684caaf01ca56d2a393b5e2
omids@DESKTOP-AMJCSCT <omid.shahidi.2012@gmail.com>
2020-11-26 23:55:28 -0500

    fixed linking issues

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,26 +18,3 @@ function navFunction() {
     }
     
 }
-
-
-//for redirection to a new page, socials footer
-function redirect(val){
-    switch(val){
-        case 0:
-            window.open("https://linkedin.com/in/omid-shahidi", "_blank");
-            break;
-        case 1:
-            window.open("https://github.com/TheOmid", "_blank");
-            break;
-        case 2:
-            window.open("https://twitter.com/u_omid", "_blank");
-            break;
-        case 3:
-            window.open("mailto: omidsh@umich.edu", "_blank");
-            break;
-        case 4:
-            window.open("https://open.spotify.com/user/22fgjchkktikjz2zpeg62uydq?si=WpA5Qsk9S4GJrzcTHihAeQ", "_blank");
-            break;
-    }
-            
-}
