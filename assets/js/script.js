caf4dfa631cc6e74f96c1b8f6f5209c5a23e7f06
TheOmid <omid.shahidi.2012@gmail.com>
2021-01-03 22:46:47 -0500

    preload js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+//p5.js
+let count = 100;
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight / 5);
+  canvas.parent("wave-animation");
+  background("#201F1F");
+}
+
+function draw() {
+  noFill();
+  
+  if(count < 400){
+    beginShape();
+    stroke(count / 5, 255 - count * 0.5, 200, 100);
+    for(x = 0; x < width; x++){
+      curveVertex(x, map(noise((x) * 0.005, frameCount * 0.005), 0, 1, 0, height));
+    }
+    endShape();
+    count++;
+    
+  }
+}
+
+
 //fixing refresh issues
 window.beforeunload = function () {
      window.scrollTo(0, 0);
@@ -14,29 +38,4 @@ function closeNav() {
 function openNav(){
     document.getElementById("navbar").style.height = "100%";
     document.getElementById("open-menu").style.display = "none";
-}
-
-//p5.js
-let count = 0;
-function setup() {
-  let canvas = createCanvas(windowWidth, windowHeight / 5);
-  canvas.parent("wave-animation");
-  background("#201F1F");
-}
-
-function draw() {
-  noFill();
-  
-  if(count < 400){
-    beginShape();
-    stroke(count / 5, 255 - count * 0.5, 200, 100);
-    for(x = 0; x < width; x++){
-      let n = noise(x * 0.005, frameCount * 0.005);
-      let y = map(n, 0, 1, 0, height);
-      curveVertex(x, y);
-    }
-    endShape();
-    count++;
-    
-  }
 }
